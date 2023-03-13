75d21e6bc15848f704c9ffc5424d3211b48e087c
lakhansolanki1234 <127746681+lakhansolanki1234@users.noreply.github.com>
2023-03-13 17:22:14 +0530

    3 files added

---
+++ src/aapp/aapp.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-aapp',
+  templateUrl: './aapp.component.html',
+  styleUrls: ['./aapp.component.css']
+})
+export class AappComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
